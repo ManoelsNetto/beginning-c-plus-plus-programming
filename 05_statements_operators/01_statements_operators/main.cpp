3b5f6e9eae900c769cd30102959b485814147456
Sir Manoel <manoel.silva.neto2013@gmail.com>
2024-04-07 16:45:42 -0300

    update 07-04-2024

---
+++ 05_statements_operators/01_statements_operators/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int num1 {10};
+    int num2 {20};
+    
+    num1 = 100;
+	
+    cout << "Num1 is " << num1 << endl;
+    cout << "Num2 is " << num2 << endl;
+    
+	return 0;
+
+};
