7eaf59f23a4940a482e8be9af0b8b163c5411df5
Sir Manoel <manoel.silva.neto2013@gmail.com>
2024-04-29 00:05:04 -0300

    29-04-2024 update

--- 06_controlling_program_flow/10_challenge/main.cpp
+++ 06_controlling_program_flow/10_challenge/main.cpp
@@ -62,3 +62,182 @@ Additional functionality if you wish to extend this program.
 Good luck!
 
 */
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    char selection{};
+    vector<int> list{};
+
+    do {
+
+        cout << "\nP - Print numbers\nA - Add a number\nM - Display mean of the numbers\nS - Display the smallest number\nL - Display the largest number\nF - Search a number\nC - Clear list\nQ - Quit" << endl;
+        cout << "\nEnter your choice: ";
+        cin >> selection;
+
+        if (selection == 'P' || selection == 'p') {
+
+            if (list.size() > 0) {
+
+                cout << "[";
+
+                for (int elem : list) {
+
+                    cout << " " << elem;
+
+                }
+
+                cout << " ]" << endl;
+
+            }
+            else {
+
+                cout << "[] - the list is empty" << endl;
+
+            }
+
+        }
+        else if (selection == 'A' || selection == 'a') {
+
+            int number{};
+            cout << "Integer to add to the list: ";
+            cin >> number;
+            cout << endl;
+
+            list.push_back(number);
+            cout << number << " added" << endl;
+        
+        }
+        else if (selection == 'M' || selection == 'm') {
+
+            if (list.size() > 0) {
+
+                int sum{};
+
+                for (int elem : list) {
+
+                    sum += elem;
+
+                }
+
+                cout << "Mean: " << static_cast<double>(sum) / list.size() << endl;
+
+            }
+            else {
+
+                cout << "Unable to calculate the mean - no data" << endl;
+
+            }
+
+        }
+        else if (selection == 'S' || selection == 's') {
+
+            if (list.size() > 0) {
+
+                int smallest{};
+
+                for (int i = { 0 }; i < list.size(); i++) {
+
+                    if (i == 0) {
+
+                        smallest = { list.at(i) };
+                        continue;
+                    }
+
+                    if (list[i] < smallest) {
+
+                        smallest = { list.at(i) };
+                        continue;
+
+                    }
+
+                }
+
+                cout << "The smallest number is " << smallest << endl;
+
+            }
+            else {
+
+                cout << "Unable to determine the smallest number - list is empty" << endl;
+
+            }
+
+        }
+        else if (selection == 'L' || selection == 'l') {
+
+            if (list.size() > 0) {
+
+                int largest{};
+
+                for (int i = { 0 }; i < list.size(); i++) {
+
+                    if (i == 0) {
+
+                        largest = { list.at(i) };
+                        continue;
+                    }
+
+                    if (list[i] > largest) {
+
+                        largest = { list.at(i) };
+                        continue;
+
+                    }
+
+                }
+
+                cout << "The largest number is " << largest << endl;
+
+            }
+            else {
+
+                cout << "Unable to determine the largest number - list is empty" << endl;
+
+            }
+
+        }
+        else if (selection == 'Q' || selection == 'q') {
+            
+            cout << "Goodbye!" << endl;
+            break;
+        
+        }
+        else if (selection == 'F' || selection == 'f') {
+
+            int number{}, times{};
+            cout << "Integer to search in the list: ";
+            cin >> number;
+            cout << endl;
+
+            for (int elem : list) {
+
+                if (elem == number) {
+
+                    times++;
+
+                }
+
+            }
+
+            cout << "Number of times " << number << " occurs in the list: " << times << endl;
+            
+        }
+        else if (selection == 'C' || selection == 'c') {
+
+            cout << "The list is empty again" << endl;
+            list.clear();
+
+        }
+        else {
+
+            cout << "Unknown selection, please try again" << endl;
+
+        }
+
+    } while (true);
+
+    return 0;
+
+}
